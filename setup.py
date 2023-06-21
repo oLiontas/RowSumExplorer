8da1d77b2f2a570e6308667f3c2e8586683f4ae1
Leonidas Dosas <liontas76@gmail.com>
2023-06-21 12:43:02 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rowSumExplorer',
+    version='1.0',
+    author='Leonidas Dosas',
+    author_email='liontas76@gmail.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/oLiontas/RowSumExplorer/code',
+    project_urls = {
+        ""
+    },
+    license='MIT',
+    packages=['code'],
+    install_requires=['requests'],
+)
